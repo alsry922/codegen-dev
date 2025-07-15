aeb3d95c8069b88ad4d6259c92e9d7924b93ec26
alsry922 <alsry922@gmail.com>
2025-07-15 14:28:47 +0900

    gitignore 추가

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
-import { commands, ExtensionContext } from "vscode";
-import { HelloWorldPanel } from "./panels/HelloWorldPanel";
+import { commands, ExtensionContext } from 'vscode';
+import { HelloWorldPanel } from './panels/HelloWorldPanel';
 
 export function activate(context: ExtensionContext) {
   // Create the show hello world command
   const showHelloWorldCommand = commands.registerCommand(
-    "codegen-dev.showHelloWorld",
+    'genai-codegen-extension.showHelloWorld',
     () => {
       HelloWorldPanel.render(context.extensionUri);
-    }
+    },
   );
 
   // Add command to the extension context
--- webview-ui/src/app/App.tsx
+++ webview-ui/src/app/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <div className="bg-amber-300">Hello</div>
     </>
   );
 }
